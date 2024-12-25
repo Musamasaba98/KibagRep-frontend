9df5f81ab15efcd192d21ac53762436a115bc68c
Musamasaba98 <masamata98@gmail.com>
2024-12-25 17:27:36 +0300

    Add user profile cards within the dates

--- kibagRep-frontend/src/componets/Sidebar/Sidebar.jsx
+++ kibagRep-frontend/src/componets/Sidebar/Sidebar.jsx
@@ -1,27 +1,21 @@
 import { useContext } from "react";
 import { AppContext } from "../../pages/context/AppContext";
-import { FaPlus } from "react-icons/fa6";
+import AccordionItem from "./AccordionItem/AccordionItem";
 
 const Sidebar = () => {
   const { data } = useContext(AppContext);
-
+  console.log(data);
   return (
     <div>
       <div className=" w-full">
         <div className=" grid gap-0.5 ">
           {data.dates.map((date, index) => (
-            <div
+            <AccordionItem
               key={index}
-              className="flex justify-between p-2 bg-[#5ac388]  text-white  text-center "
-            >
-              <div className="flex justify-between gap-5">
-                <p>{date.date}</p>
-                <p>({date.count})</p>
-              </div>
-              <div>
-                <FaPlus onClick={() => alert("Hello")} />
-              </div>
-            </div>
+              date={date.date}
+              count={date.count}
+              profiles={date.profiles || []}
+            />
           ))}
         </div>
       </div>
